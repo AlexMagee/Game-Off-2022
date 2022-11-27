b4a1d5279925fe137c9710b296d25a8e7d25536d
AlexEMagee <alex@aem2create.com>
2022-11-27 12:10:07 -0600

    Updated instructions

--- menu.lua
+++ menu.lua
@@ -70,7 +70,7 @@ function menu_draw()
         love.graphics.printf("Quit", (love.graphics.getWidth() / 2) - 150, 160, 300, "center")
     elseif menu_state == 1 then
         love.graphics.setColor(1, 1, 1)
-        love.graphics.print("Welcome to Strike While the Iron is Hot.\nYou are about to become a Metalcraftdwarf completing a series of goblet work orders.\nYou will be given an anvil, a hammer, and unrestricted access to the metal bar stockpile.\n\nPress H to swing your hammer.\nFor best results, swing while the iron is hot.\n\n\nPress Escape to Return", 100, 100)
+        love.graphics.print("Welcome to Strike While the Iron is Hot.\nYou are about to become a Metalcraftdwarf completing a series of goblet work orders.\nYou will be given an anvil, a hammer, and unrestricted access to the metal bar stockpile.\n\nYour Craftsdwarfship will be judged as either Elf-made, Decent, Fine, Exceptional, or Masterful\ndepending on how well you work the iron.\nBonus points will be given for completing orders quickly. Delays will cost you.\n\nPress H to swing your hammer.\nFor best results, swing while the iron is hot.\n\n\nPress Escape to Return", 100, 100)
     elseif menu_state == 2 then
         love.graphics.setColor(1, 1, 1)
         love.graphics.print("Game Design by Alex Magee\nArtwork by Alex Magee\nSoundtrack by Logan Hardin\nSound Effects by Alex Magee\nSpecial thanks to Amy, Kyle, and Logan\n\n\nPress Escape to Return", 100, 100)
