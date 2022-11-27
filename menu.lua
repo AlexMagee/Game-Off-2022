b47afc16325964e75e9704a9f2ac0397ed5d8708
AlexEMagee <alex@aem2create.com>
2022-11-27 17:40:48 -0600

    Edited credits

--- menu.lua
+++ menu.lua
@@ -36,7 +36,9 @@ end
 function menu_draw()
     love.graphics.setFont(menu_font)
     love.graphics.setColor(1, 0, 0)
-    love.graphics.printf("Strike While the Iron is Hot", (love.graphics.getWidth() / 2) - 150, 30, 300, "center")
+    love.graphics.printf("Workshops of the Mountainhome", (love.graphics.getWidth() / 2) - 150, 30, 300, "center")
+    love.graphics.setColor(0.5, 0.5, 0.5)
+    love.graphics.printf("Chapter I: Strike While the Iron is Hot", (love.graphics.getWidth() / 2) - 150, 50, 300, "center")
     if menu_state == 0 then
         -- Play Game
         if menu_highlight == 0 then
@@ -71,9 +73,9 @@ function menu_draw()
         love.graphics.printf("Quit", (love.graphics.getWidth() / 2) - 150, 160, 300, "center")
     elseif menu_state == 1 then
         love.graphics.setColor(1, 1, 1)
-        love.graphics.print("Welcome to Strike While the Iron is Hot.\nYou are about to become a Metalcraftdwarf completing a series of goblet work orders.\nYou will be given an anvil, a hammer, and unrestricted access to the metal bar stockpile.\n\nYour Craftsdwarfship will be judged as either Elf-made, Decent, Fine, Exceptional, or Masterful\ndepending on how well you work the iron.\nBonus points will be given for completing orders quickly. Delays will cost you.\n\nPress H to swing your hammer.\nFor best results, swing while the iron is hot.\n\n\nPress Escape to Return", 100, 100)
+        love.graphics.print("Welcome to Strike While the Iron is Hot.\nYou are about to become a Metalcraftdwarf completing a series of goblet work orders.\nYou will be given an anvil, a hammer, and unrestricted access to the metal bar stockpile.\n\nYour Craftsdwarfship will be judged as either Elf-made, Decent, Fine, Exceptional, or Masterful\ndepending on how well you work the iron.\nBonus points will be given for completing orders quickly. Delays will cost you.\n\nPress H to swing your hammer.\nFor best results, swing while the iron is hot.\n\nFor the Glory of the Mountainhome!\n\n\nPress Escape to Return", 100, 100)
     elseif menu_state == 2 then
         love.graphics.setColor(1, 1, 1)
-        love.graphics.print("Game Design by Alex Magee\nArtwork by Alex Magee\nSoundtrack by Logan Hardin\nSound Effects by Alex Magee\nSpecial thanks to Amy, Kyle, and Logan\n\n\nPress Escape to Return", 100, 100)
+        love.graphics.print("Expedition Members:\n\nARTIST Alex Magee\nCOMPOSER Logan Hardin\nGAME DESIGNER Alex Magee\nPROGRAMMER Alex Magee\n\nSpecial thanks to:\n\nAmy\nBailey\nLogan\nKyle\n\n\nPress Escape to Return", 100, 100)
     end
 end
